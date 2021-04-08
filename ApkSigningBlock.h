81e46ccd7bc5c9dacba59855b009c17ffe2280c1
z2care <z2care@users.noreply.github.com>
2021-04-08 22:41:50 +0800

    Update ApkSigningBlock.h

--- ApkSigningBlock.h
+++ ApkSigningBlock.h
@@ -8,5 +8,10 @@
 using namespace std;
 int readApkSigningBlock(fstream& infile, int sig_offset, int* sig_total_size);
 void writeApkSigningBlock(fstream& infile, fstream& outfile, int sig_offset, int sig_size);
-
+        // FORMAT:
+        // OFFSET       DATA TYPE  DESCRIPTION
+        // * @+0  bytes uint64:    size in bytes (excluding this field)
+        // * @+8  bytes pairs
+        // * @-24 bytes uint64:    size in bytes (same as the one above)
+        // * @-16 bytes uint128:   magic
 #endif
